c35e97ed9a585971b9cb2b33271dcbd338c1a87d
tglewis1993 <tglewis1993@gmail.com>
2020-01-31 23:28:04 +0000

    Adding initial unity content

---
+++ Assets/Debug/Scripts/OnScreenDebug.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI))]
+public class OnScreenDebug : MonoBehaviour
+{
+    TextMeshProUGUI debugText = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!TryGetComponent<TextMeshProUGUI>(out debugText))
+        {
+            Debug.LogError("Failed to get debug text.");
+        }
+    }
+    
+    public void PrintDebugMessage(string msg)
+    {
+        if (debugText != null)
+            debugText.text = msg;
+    }
+}
