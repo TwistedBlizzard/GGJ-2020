49b32bb765e57d4ee0463025bd47987dfe913c01
tglewis1993 <tglewis1993@gmail.com>
2020-02-01 00:23:11 +0000

    Added tool click and drag prefab. Started work on the tool manager.

---
+++ Assets/Scripts/Tools/Tool.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(RectTransform))]
+public class Tool : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    protected RectTransform m_Rect = null;
+    protected string m_ToolName = null;
+
+    // this stops the tool image from snapping to the middle of the cursor mid drag.
+    Vector2 m_Offset = Vector2.zero;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        if (m_Rect != null && eventData != null)
+        {
+            m_Offset = new Vector2(m_Rect.position.x - eventData.position.x, m_Rect.position.y - eventData.position.y);
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // drag tool to follow cursor
+        if (m_Rect != null && eventData != null)
+        {
+            m_Rect.localPosition = eventData.position + m_Offset;
+        }
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        m_Offset = Vector2.zero;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!TryGetComponent<RectTransform>(out m_Rect))
+        {
+            Debug.LogError($"Tool ({m_ToolName}) doesn't have a valid RectTransform.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Tools/ToolsManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolsManager : MonoBehaviour
+{
+    List<Tool> m_ToolsList = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_ToolsList = new List<Tool>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
